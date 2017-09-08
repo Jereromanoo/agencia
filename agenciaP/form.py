b0024e1d054f430f27783f010ac2d7dc1a4a8b8e
thevegit0 <taieltraversa@gmail.com>
2017-09-08 11:15:26 -0300

    Forms

---
+++ agenciaP/form.py
@@ -0,0 +1,9 @@
+from django import forms
+
+    from .models import Desocupado
+
+    class DesocupadoForm(forms.ModelForm):
+
+        class Meta:
+            model = Desocupado
+            fields = ('nombre', 'text',)
--- agenciaP/models.py
+++ agenciaP/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
-        author = models.ForeignKey('auth.User')
-        title = models.CharField(max_length=200)
-        text = models.TextField()
-        created_date = models.DateTimeField(
-                default=timezone.now)
-        published_date = models.DateTimeField(
-                blank=True, null=True)
+class Desocupado(models.Model):
+        nombre = models.CharField(max_length=20)
+        apellido = models.CharField(max_length=20)
+        dni = models.CharField(8)
+        fechaDeNacimiento = models.CharField(max_length=20)
+        experienciaLaboral = models.TextField(max_length=200)
+        formacion = models.TextField(max_length=200)
+        habilidades = models.TextField(max_length=200)
+
 
 def publish(self):
             self.published_date = timezone.now()
