741b4044ee3d578b2aee7d2293c10e7cad0dd930
jereromanoo <jere0699@gmail.com>
2017-09-10 19:32:32 -0300

    Boostrap y Css

--- agencia/urls.py
+++ agencia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', include('agenciaP.urls', namespace="udesocupados")),
+    url(r'^(?!.)', include('agenciaP.urls', namespace="udesocupados")),
 ]
--- agenciaP/urls.py
+++ agenciaP/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
         url(r'^desocupados', views.DesocupadoList.as_view(), name="desocupados"),
-        url(r'^registrar', views.add_desocupado, name="dadd"),
+        url(r'^(?!.)', views.add_desocupado, name="dadd"),
     ]
